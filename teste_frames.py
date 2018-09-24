2ab55ca0db5287abcc6f37836e07012855907681
Marina <marinastavares6@gmail.com>
2018-09-24 11:57:05 -0300

    novos blobs
    adicionar novos blobs fora do frame 0

--- teste_frames.py
+++ teste_frames.py
@@ -3,8 +3,8 @@ import cv2
 globalDistance = 130
 numberFrame = -1
 
-cap = cv2.VideoCapture('video2.MOV')
-cap.set(1, 480)
+cap = cv2.VideoCapture("/Users/marinastavares/video5.mp4")
+# cap.set(1, 100)
 
 font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
 
@@ -29,6 +29,19 @@ class KiloBlobs:
 
 # Lista onde os kilobots estarão armazenadas
 ListaBlob = []
+Encontrados = []
+
+class blobsEncontrados:
+    def __init__(self,x,y,tamanho,match):
+        self.x = x
+        self.y =  y
+        self.tamanho = tamanho
+        self.match = match
+
+    def atualizarMatch(self,match):
+        self.match = match
+pass
+
 
 def minDistance(x1,y1,x2,y2):
     distx = (x1 - x2) ** 2
@@ -37,20 +50,25 @@ def minDistance(x1,y1,x2,y2):
     return d
 
 i = 0
+ListaMatch = []
 
 while True:
     
     ret, frame = cap.read()
     numberFrame = numberFrame + 1
-    frame = cv2.resize(frame, (300,300))
+    frame = cv2.resize(frame, (200,200))
     detector = cv2.SimpleBlobDetector_create()
     keypoints = detector.detect(frame)
     im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]),(0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)  
 
+    for detectados in keypoints:
+        encontrado = blobsEncontrados(detectados.pt[0],detectados.pt[1],detectados.size,False)
+        Encontrados.append(encontrado)
+
     ## cria lógica para Frame 0
     if (numberFrame == 0):
-        for k in keypoints: 
-            blob=KiloBlobs(i,k.pt[0],k.pt[1],k.size)
+        for k in Encontrados: 
+            blob=KiloBlobs(i,k.x,k.y,k.tamanho)
             ListaBlob.append(blob)
             print('Primeiros Blobs criados')
             ListaBlob[i].printarInfos()
@@ -62,22 +80,30 @@ while True:
         # compara keypoints de um frame com os keypoints do frame anterior (tentar todas combinacoes) 
         # n = listablob0 , n = listablob1
         a=0
-        print('Novo frame')
-        for n in ListaBlob:
+        # print('Novo frame')
+        for l in Encontrados:
             # contador l
-            for l in keypoints:
-                # print(n.ID)
-                dist = minDistance(l.pt[0],l.pt[1],n.x,n.y)
-                print('X,Y DO KEYPOINF %.1f %.1f E DO BLOB %.1f %.1f' % (l.pt[0],l.pt[1],n.x,n.y))
-                print('A comparacao entre o blob FIXO %d e %d deu uma distancia %.1f, o tamanho do %.1f' % (n.ID,a,dist,n.tamanho))
+            for n in ListaBlob:
+                # Calcular a distancia
+                dist = minDistance(l.x,l.y,n.x,n.y)
+                
                 if (dist < n.tamanho):
                     print('Atualizar o ID %d' % (n.ID))
-                    n.atualizarXY(l.pt[0],l.pt[1],l.size)
+                    n.atualizarXY(l.x,l.y,l.tamanho)
                     n.printarInfos()
                     cv2.putText(im_with_keypoints,str(n.ID),(int(n.x),int(n.y)), font, 1, (200,0,0), 2, cv2.LINE_AA)
-                a=a+1
+                    l.atualizarMatch(True)
 
-
+    if (len(Encontrados) > len(ListaBlob)):
+        for perdido in Encontrados:
+            if (perdido.match == False):
+                blob = KiloBlobs(i,perdido.x,perdido.y,perdido.tamanho)
+                ListaBlob.append(blob)
+                cv2.putText(im_with_keypoints,str(blob.ID),(int(blob.x),int(blob.y)), font, 1, (200,0,0), 2, cv2.LINE_AA)
+                i+=1
+    
+    Encontrados = []
+                
     cv2.imshow("Video", im_with_keypoints)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
